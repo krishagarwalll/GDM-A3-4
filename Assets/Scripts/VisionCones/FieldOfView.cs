c9c92c91bc442afa28e971779e40ad1d8910b590
krishagarwalll <krish.agarwal@student.uts.edu.au>
2026-04-11 17:45:11 +1000

    Field of View now works

--- Assets/Scripts/VisionCones/FieldOfView.cs
+++ Assets/Scripts/VisionCones/FieldOfView.cs
@@ -4,16 +4,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FieldOfView : MonoBehaviour {
-    private void Start() {
-        Mesh mesh = new Mesh();
+    
+    [SerializeField] private LayerMask layerMask;
+    private Mesh mesh;
+
+    private void Start()
+    {
+        mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
-        
-        float fov = 60f;
+    }
+
+    private void Update() {
+        float fov = 40f;
         Vector3 origin = Vector3.zero;
         int rayCount = 30;
         float angle = 0f;
         float angleIncrease = fov / rayCount;
-        float viewDistance = 50f;
+        float viewDistance = 3f;
         
         Vector3[] vertices = new Vector3[rayCount +1 +1];
         Vector2[] uv = new Vector2[vertices.Length];
@@ -23,12 +30,24 @@ public class FieldOfView : MonoBehaviour {
 
         int vertexIndex = 1;
         int triangleIndex = 0;
-        for (int i = 0; i <= rayCount; i++) {
-            Vector3 vertex = origin + getVectorFromAngle(angle) * viewDistance;
+        for (int i = 0; i <= rayCount; i++)
+        {
+            Vector3 vertex;
+            Vector3 direction = getVectorFromAngle(angle);
+            Vector3 worldDirection = transform.TransformDirection(direction);
+            RaycastHit2D raycastHit2D = layerMask.value == 0
+                ? Physics2D.Raycast(transform.position, worldDirection, viewDistance)
+                : Physics2D.Raycast(transform.position, worldDirection, viewDistance, layerMask);
+            
+            if (raycastHit2D.collider == null) {
+                vertex = origin + direction * viewDistance;
+            } else {
+                vertex = transform.InverseTransformPoint(raycastHit2D.point);
+            }
             vertices[vertexIndex] = vertex;
             
             if (i > 0) {
-                triangles[triangleIndex] = 0;
+                triangles[triangleIndex + 0] = 0;
                 triangles[triangleIndex + 1] = vertexIndex - 1;
                 triangles[triangleIndex + 2] = vertexIndex;
 
