458823ae93ff5bb447ad18424f5698a159121ed2
krishagarwalll <krish.agarwal@student.uts.edu.au>
2026-04-14 01:14:10 +1000

    Create Enemy Ai State Machine Template

--- Assets/Scripts/Enemy/EnemyAI.cs
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -1,5 +1,39 @@
+using System;
 using UnityEngine;
 
 public class EnemyAI : MonoBehaviour {
+    private enum State {
+        Patrol,
+        Suspicious,
+        Alert,
+        Search,
+        KnockedOut
+    }
+
+    private State state;
     
+    private void Awake()
+    {
+        
+    }
+
+    private void Update() { //Todo
+        switch (state) {
+            case State.Patrol:
+                break;
+            case State.Suspicious:
+                break;
+            case State.Alert:
+                break;
+            case State.Search:
+                break;
+            case State.KnockedOut:
+                break;
+        }
+    }
+
+    private void ChangeState(State newState)
+    {
+        //Todo
+    }
 }
