3fdb95f71c602f79ea01e6c28c4416c821731dc3
christie <gaiaapollo42@gmail.com>
2026-04-15 16:44:07 +1000

    Manually Adding changes to startMenu to Dev 2

--- Assets/Scripts/UI/GameSceneManager.cs
+++ Assets/Scripts/UI/GameSceneManager.cs
@@ -1,57 +1,74 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class GameSceneManager : MonoBehaviour
 {
-    public enum SceneIndex
+    public void LoadFirstLevel()
     {
-        MainMenu = 0,
-        LevelSelect   = 1,
-        Level0   = 2
-    }
-
-    public static GameSceneManager Instance { get; private set; }
-
-    void Awake()
-    {
-        if (Instance != null) { Destroy(gameObject); return; }
-        Instance = this;
         DontDestroyOnLoad(gameObject);
+        SceneManager.LoadScene("tutorialLevel");
     }
 
-    void Update()
-    {
-        var keyboard = Keyboard.current;
-        if (keyboard == null) return;
+}
 
-        if (keyboard.rKey.wasPressedThisFrame)
-            ReloadCurrentScene();
-    }
+//using System.Collections;
+//using UnityEngine;
+//using UnityEngine.InputSystem;
+//using UnityEngine.SceneManagement;
+
+//public class GameSceneManager : MonoBehaviour
+//{
+    //public enum SceneIndex
+    //{
+        //MainMenu = 0,
+        //LevelSelect   = 1,
+        //Level0   = 2
+    //}
+
+    //public static GameSceneManager Instance { get; private set; }
+
+    //void Awake()
+    //{
+        //if (Instance != null) { Destroy(gameObject); return; }
+        //Instance = this;
+        //DontDestroyOnLoad(gameObject);
+    //}
+
+    //void Update()
+    //{
+        //var keyboard = Keyboard.current;
+        //if (keyboard == null) return;
+
+        //if (keyboard.rKey.wasPressedThisFrame)
+            //ReloadCurrentScene();
+    //}
 
     // For use in code
-    public void LoadScene(SceneIndex scene)
-    {
-        StartCoroutine(LoadRoutine((int)scene));
-    }
+    //public void LoadScene(SceneIndex scene)
+    //{
+        //StartCoroutine(LoadRoutine((int)scene));
+    //}
 
     // For use on UI buttons in the Inspector
-    public void LoadScene(int buildIndex)
-    {
-        StartCoroutine(LoadRoutine(buildIndex));
-    }
+    //public void LoadScene(int buildIndex)
+    //{
+        //StartCoroutine(LoadRoutine(buildIndex));
+    //}
 
-    public void ReloadCurrentScene()
-    {
-        StartCoroutine(LoadRoutine(SceneManager.GetActiveScene().buildIndex));
-    }
+    //public void ReloadCurrentScene()
+    //{
+        //StartCoroutine(LoadRoutine(SceneManager.GetActiveScene().buildIndex));
+    //}
 
-    private IEnumerator LoadRoutine(int buildIndex)
-    {
-        AsyncOperation op = SceneManager.LoadSceneAsync(buildIndex);
+    //private IEnumerator LoadRoutine(int buildIndex)
+    //{
+        //AsyncOperation op = SceneManager.LoadSceneAsync(buildIndex);
 
-        while (!op.isDone)
-            yield return null;
-    }
-}
+        //while (!op.isDone)
+            //yield return null;
+    //}
+//}
